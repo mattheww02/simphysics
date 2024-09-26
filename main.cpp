827134d2c30edf01f7643334c8e92116724a065d
mattheww02 <78615724+mattheww02@users.noreply.github.com>
2024-09-26 02:04:46 +0100

    Initial setup

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+int main() {
+    if (!glfwInit()) {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;  // Exit if initialization fails
+    }
+    return 0;
+}
